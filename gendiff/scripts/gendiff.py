b87c3663e65288abd3eb18866d3a58b02e863e53
mrJonson <mr.Jonson@mail.ru>
2022-01-10 03:09:37 +0400

    add step 2 of python-project-lvl2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,6 +8,9 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', type=str, help='')
     parser.add_argument('second_file', type=str, help='')
+    parser.add_argument('-f', '--format', choices=['json', 'plain', 'stylish'],
+                        default='stylish',
+                        help='set format of output. stylish is default')
     args = parser.parse_args()
     print(args.indir)
 
