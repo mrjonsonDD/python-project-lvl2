5f48a1bfcda1b292cf575c2bfe41c96837b48d07
mrJonson <mr.Jonson@mail.ru>
2022-03-23 11:50:30 +0400

    fix format_stylish

--- gendiff/formatters/format_stylish.py
+++ gendiff/formatters/format_stylish.py
@@ -27,47 +27,44 @@ def to_slylish(diff):
 
 
 def stringify_node(item, depth):
-    replacer = REPLACER
-    space_count = SPACE_CNT
     key, item_value = item
     item_type = item_value.get(TYPE)
-    deep_indent_size = depth + space_count
-    deep_indent = replacer * deep_indent_size
     item_str = ''
-
+    indent = depth + 2
+    indent_str = REPLACER * indent
+    
     if item_type == NESTED:
-        children = item_value.get(CHILDREN)
-        indent = replacer * (depth + 2)
+        children = item_value.get(CHILDREN)   
         new_lines = [
-            stringify_node(child, depth + 2) 
+            stringify_node(child, indent) 
             for child in list(children.items())
         ]
-        child_str = '{\n' + '\n'.join(new_lines) + '\n' + indent + '}'
-        item_str = f"{deep_indent}  {key}: {child_str}"
+        child_str = '{\n' + '\n'.join(new_lines) + '\n' + indent_str + '}'
+        item_str = f"{REPLACER * (depth + 1)}  {key}: {child_str}"
         return item_str
 
     if item_type == UPDATED:
         value1 = item_value.get(OLD_VAL)
         value2 = item_value.get(NEW_VAL)
-        val1 = stringify_value(value1, depth + 2, REPLACER, SPACE_CNT)
-        val2 = stringify_value(value2, depth + 2, REPLACER, SPACE_CNT)
-        res_str1 = f"{deep_indent}- {key}: {val1}"
-        res_str2 = f"{deep_indent}+ {key}: {val2}"
+        val1 = stringify_value(value1, indent, REPLACER, SPACE_CNT)
+        val2 = stringify_value(value2, indent, REPLACER, SPACE_CNT)
+        res_str1 = f"{REPLACER * (depth + 1)}- {key}: {val1}"
+        res_str2 = f"{REPLACER * (depth + 1)}+ {key}: {val2}"
         item_str = res_str1 + '\n' + res_str2
         return item_str
 
     value = item_value.get(VALUE)
-    val = stringify_value(value, depth + 2, REPLACER, SPACE_CNT)
+    val = stringify_value(value, indent, REPLACER, SPACE_CNT)
 
     if item_type == UNCHANGED:
-        return f"{deep_indent}  {key}: {val}"
+        return f"{REPLACER * (depth + 1)}  {key}: {val}"
 
     if item_type == REMOVED:
-        item_str = f"{deep_indent}- {key}: {val}"
+        item_str = f"{REPLACER * (depth + 1)}- {key}: {val}"
         return item_str
 
     if item_type == ADDED:
-        item_str = f"{deep_indent}+ {key}: {val}"
+        item_str = f"{REPLACER * (depth + 1)}+ {key}: {val}"
         return item_str    
 
 
